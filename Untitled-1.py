df6f993e6c2c089a3cf7ea00fb33425747353036
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-02-18 22:05:21 -0500

    extra comments + class restructuring

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,20 @@
 from chatgpt_wrapper import ChatGPT
 
-bot = ChatGPT()
-response = bot.ask("")
-print(response)
+
+class bot_call():
+    def __init__(self, comment):
+        global response
+        bot_call.bot = ChatGPT()
+        bot_call.response = bot_call.bot.ask(f"Give me a witty reply to this reddit comment: '{comment}'. Please keep it one to two sentences long.")
+        response = bot_call.response
+        print(response)
+
+
+TEST = bot_call("Manchester United is a bigger club than Manchester City")
+
+
+# set limit of witty comments to two per reddit thread
+# focus on subreddits such as ShowerThoughts, Soccer etc.
+
+# experiment will be of two parts, one focusing on short comments (such as r/showerthoughts) and others focusing on long paragraphs (such as r/subredditdrama). 
+# i hypothesize that it'll do better 
