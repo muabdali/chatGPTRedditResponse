2634a5f6bc5a477916be8f4a3b18fbff0bd1fe4f
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-02-27 23:39:46 -0500

    x

--- reddit-Bot.py
+++ reddit-Bot.py
@@ -7,3 +7,11 @@ reddit = praw.Reddit(
     user_agent="",
     username="",
 )
+
+
+# needs submission number from comment link
+submission = reddit.submission("3g1jfi")
+
+# takes highest comment
+for top_level_comment in submission.comments:
+    print(top_level_comment.body)
