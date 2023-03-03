7bea6aad10f6a1dea7a414bfc557a16782af615e
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-03-02 23:52:43 -0500

    new way of getting secret tokens

--- reddit-Bot.py
+++ reddit-Bot.py
@@ -1,5 +1,30 @@
 from praw import reddit as praw 
-import re
+
+
+with open('test.txt', 'r') as file:
+    # Read the contents of the file
+    contents = file.readlines()
+
+
+for line in contents:
+    # Check if the line contains "client_id"
+    if "client_id" in line:
+        # Split the line on the = character
+        parts = line.split('=')
+        # Get the second part (the value) and remove any whitespace
+        client_id = parts[1].strip()
+
+    # Check if the line contains "client_code"
+    elif "client_code" in line:
+        # Split the line on the = character
+        parts = line.split('=')
+        # Get the second part (the value) and remove any whitespace
+        client_code = parts[1].strip()
+
+# Print the results
+print(client_id)
+print(client_code)
+
 
 
 reddit = praw.Reddit(
