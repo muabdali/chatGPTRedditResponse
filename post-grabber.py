0a5b1d10676562e005408e85d2101c6cd73286ef
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-03-09 21:31:13 -0500

    added

--- post-grabber.py
+++ post-grabber.py
@@ -1,2 +1,17 @@
+# brings all tokens, client id etc into file
 from redditTokenInit import *
+import praw
 
+print(client_secret)
+
+reddit = praw.Reddit(
+    client_id = client_id,
+    client_secret  = client_secret,
+    password = account_password,
+    user_agent = "TEST by u/DingDingPowow",
+    username = account_name
+)
+
+
+
+subreddit = reddit.subreddit("")
