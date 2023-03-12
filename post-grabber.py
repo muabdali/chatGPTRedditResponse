9d25d397d73e01d46b18704941064fb9a937b7de
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-03-12 16:09:36 -0400

    restructure post-grabber to be less convoluted

--- post-grabber.py
+++ post-grabber.py
@@ -1,26 +1,31 @@
-# brings all tokens, client id etc into file
 from redditTokenInit import *
 import praw
 
 reddit = praw.Reddit(
-    client_id = client_id,
-    client_secret  = client_secret,
-    password = account_password,
-    user_agent = "TEST by u/DingDingPowow",
-    username = account_name
+    client_id=client_id,
+    client_secret=client_secret,
+    password=account_password,
+    user_agent="TEST by u/DingDingPowow",
+    username=account_name
 )
 
 
-    
 class postGrabber:
-    def __init__(self, subredditlist) -> None:
-        def subredditlistInit(subredditlist):
-            with open(subredditlist, 'r') as file:
-                contents = file.readlines()
-            for subredditName in contents:
-                print(f'r/{subredditName}')
-        pass
+    def __init__(self, subredditlist):
+        self.subredditlist = subredditlist
+        self.subreddits = self.subredditlist_init()
 
+    def subredditlist_init(self):
+        with open(self.subredditlist, 'r') as file:
+            contents = file.readlines()
+        subreddits = []
+        for subredditName in contents:
+            subreddit = reddit.subreddit(subredditName.strip())
+            subreddits.append(subreddit)
+            print(f'r/{subredditName.strip()}')
+        return subreddits
 
 
 run = postGrabber('subredditlist.txt')
+targetList = run.subreddits
+print(targetList.strip())
